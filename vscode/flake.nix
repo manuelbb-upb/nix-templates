3c7e91213c206a5f2105552aeb38f1842457ae84
manuelbb-upb <manuelbb@mail.uni-paderborn.de>
2024-11-25 16:44:39 +0100

    vscode with defaults

---
+++ vscode/flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "VSCode in shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    nix-vscode-extensions = {
+      url = "github:nix-community/nix-vscode-extensions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
+  outputs = { 
+    self, 
+    nixpkgs, 
+    flake-utils, 
+    nix-vscode-extensions 
+  }@inputs: flake-utils.lib.eachDefaultSystem (system:
+    let 
+      pkgs = nixpkgs.legacyPackages.${system}; 
+      vs-marketplace-extensions = nix-vscode-extensions.extensions.${system}.vscode-marketplace;
+      default-extensions =  with vs-marketplace-extensions; [
+        # Spacemacs and Vim-Mode
+        vspacecode.whichkey
+        vspacecode.vspacecode
+        vscodevim.vim
+        # Random word generator
+        thmsrynr.vscode-namegen
+        # Catppuccin Theme
+        catppuccin.catppuccin-vsc
+        # Nix language extension
+        jnoortheen.nix-ide
+        # direnv chooser:
+        mkhl.direnv
+      ];
+      vscodium-with-default-extensions = pkgs.vscode-with-extensions.override {
+        vscode = pkgs.vscodium;
+        vscodeExtensions = default-extensions; 
+      };
+    in
+    {
+
+      packages = {
+        inherit vscodium-with-default-extensions;
+      };
+
+      devShells = {
+        default = pkgs.mkShell {
+          packages = (with pkgs; [
+          ]) ++ [
+            (vscodium-with-default-extensions.override ( prev: {
+              vscodeExtensions = prev.vscodeExtensions ++ (with vs-marketplace-extensions; [
+              ]);
+            }))
+          ];
+        };
+      }; 
+    }
+  );
+}
